f9dbe298043c6d85ac522483d77f0e1bc5c4b3f5
Olga <sinickaya1986@gmail.com>
2023-05-21 18:42:56 +0300

    первое решение

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+using System; //вызов библиотеке
+System.Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (number !=0)  // пока намбер не равно нулю
+ {
+    int digit = number % 10; //намбер остаток от деление на 10
+    sum += digit;  // sum = sum + digit
+    number /= 10; // number деление на 10
+}
+Console.WriteLine ("Сумма цифр в числе: " +sum);
---
+++ task29/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+//  и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.Write("Введите длину массива: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите левую границу массива: ");
+        int left = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите правая границу массива: ");
+        int right = Convert.ToInt32(Console.ReadLine());
+
+        int[] nums = GetRandomArray(size, left, right);
+
+        WriteArray(nums);
+        //Console.WriteLine(nums);
+    }
+
+    public static int[] GetRandomArray(int size, int left, int right)
+    {
+        Random rand = new Random();
+        int[] nums = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            nums[i] = rand.Next(left, right);
+        }
+
+        return nums;
+    }
+
+    public static void WriteArray(int[] input)
+    {
+        for (int i = 0;i < input.Length;i++)
+        {
+            Console.Write(input[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
